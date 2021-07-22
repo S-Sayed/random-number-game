b725fcd5006b2fd9b7f2f6ab50bef6e9517488ab
Sameh Elsayed <ssayed7190@gmail.com>
2021-07-22 21:17:47 +0400

    Create random-number-game spring boot project

---
+++ src/main/java/com/ssayed/RandomNumberGameApplication.java
@@ -0,0 +1,13 @@
+package com.ssayed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNumberGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomNumberGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssayed/RandomNumberGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssayed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
