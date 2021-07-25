3ba4e152291a4de7eec29d11a27d27dcba2c2240
Sameh Elsayed <ssayed7190@gmail.com>
2021-07-26 03:44:44 +0400

    Get main number manually/ automatically to start the game

--- src/main/java/com/ssayed/RandomNumberGameApplication.java
+++ src/main/java/com/ssayed/RandomNumberGameApplication.java
@@ -1,13 +1,42 @@
 package com.ssayed;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+
+import com.ssayed.service.RandomNumberGameService;
 
 @SpringBootApplication
-public class RandomNumberGameApplication {
+public class RandomNumberGameApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RandomNumberGameApplication.class);
+
+	@Value("${initiator: false}")
+	private Boolean initiator;
+
+	@Autowired
+	private RandomNumberGameService randomNumberGameService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandomNumberGameApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("am I the initiator of the game <{}>", initiator);
+
+		if (initiator) {
+			startTheGame();
+		}
+	}
+
+	@Async
+	private void startTheGame() throws Exception {
+		int mainNumber = randomNumberGameService.getNumber();
+		randomNumberGameService.callOtherPlayer(mainNumber);
+	}
 }
